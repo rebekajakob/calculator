9142d9ffb2e79bad81d3c542e9705048b9c40580
rebekajakob <rebusacka@gmail.com>
2022-12-17 20:53:21 +0100

    create spring boot project

---
+++ src/main/java/hu/rebekajakob/calculator/CalculatorApplication.java
@@ -0,0 +1,13 @@
+package hu.rebekajakob.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/rebekajakob/calculator/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.rebekajakob.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
