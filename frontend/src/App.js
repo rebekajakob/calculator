389c2f99ef323d4ccb22e6cc9ef6c75ca6b17014
rebekajakob <rebusacka@gmail.com>
2022-12-19 18:47:51 +0100

    place NumberButtons and ActionButtons next to each other

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Button from "./components/Button";
 import NumberButtons from "./components/NumberButtons";
 import Footer from "./components/Footer";
+import ActionButtons from "./components/ActionButtons";
 
 function App() {
     const number = 1
@@ -11,7 +12,12 @@ function App() {
   return (
     <div className="flex flex-col min-h-screen">
         <div className="grid grid-cols-5 gap-4">
-            <NumberButtons/>
+            <div className="col-span-3">
+                <NumberButtons/>
+            </div>
+            <div className="col-span-2">
+                <ActionButtons/>
+            </div>
         </div>
         <Footer/>
     </div>
--- frontend/src/components/NumberButtons.js
+++ frontend/src/components/NumberButtons.js
@@ -18,10 +18,8 @@ const NumberButtons = () => {
 
     const buttons = createNumberButtons();
     return (
-        <div class="col-span-3">
         <div className="grid grid-cols-3 gap-4">
             {buttons}
-        </div>
         </div>
     )
 }
