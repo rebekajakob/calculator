27ef5572bfaf986176afe94364f117fff5963fdf
rebekajakob <rebusacka@gmail.com>
2022-12-21 13:44:54 +0100

    create DisplayRow with styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import Button from "./components/Button";
 import NumberButtons from "./components/NumberButtons";
 import Footer from "./components/Footer";
 import ActionButtons from "./components/ActionButtons";
+import DisplayRow from "./components/DisplayRow";
 
 function App() {
     const number = 1
@@ -11,7 +12,10 @@ function App() {
     }
   return (
     <div className="flex flex-col min-h-screen">
-        <div className="grid grid-cols-5 gap-4">
+        <div className="grid grid-cols-5 gap-4 px-4">
+            <div className="col-span-5 ">
+                <DisplayRow text={"ola!"}/>
+            </div>
             <div className="col-span-3">
                 <NumberButtons/>
             </div>
